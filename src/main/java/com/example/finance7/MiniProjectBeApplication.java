9f83a45d2717efb0683a883ae7bee500036dc04e
cocoiscat <demy176@naver.com>
2023-02-27 16:10:58 +0900

    Initial Commit

---
+++ src/main/java/com/example/finance7/MiniProjectBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.finance7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MiniProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectBeApplication.class, args);
+    }
+
+}

16f2929aa4490be406ac62a6bef407fd82dadac5
cocoiscat <demy176@naver.com>
2023-03-20 23:17:55 +0900

    fix: 상품 ID 입력시 유효성 검사
    무조건 양수로만 입력 받도록 함

--- src/main/java/com/example/finance7/cart/dto/CartRequestDTO.java
+++ src/main/java/com/example/finance7/cart/dto/CartRequestDTO.java
@@ -2,8 +2,7 @@ package com.example.finance7.cart.dto;
 
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,7 +11,6 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class CartRequestDTO {
 
-    @NotBlank(message = "공백은 입력받을 수 없습니다.")
-    @Pattern(regexp = "^[0-9]*$", message = "상품 ID는 숫자만 입력받을 수 있습니다.")
+    @Positive
     private Long productId;
 }

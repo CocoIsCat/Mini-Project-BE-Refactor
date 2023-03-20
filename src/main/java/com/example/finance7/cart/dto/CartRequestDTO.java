2541214ec7ca1ae64f80c0e521797c5dceb4ee47
cocoiscat <demy176@naver.com>
2023-03-20 15:03:21 +0900

    feat: 입력에 대한 유효성 검사를 진행한다.

--- src/main/java/com/example/finance7/cart/dto/CartRequestDTO.java
+++ src/main/java/com/example/finance7/cart/dto/CartRequestDTO.java
@@ -2,11 +2,17 @@ package com.example.finance7.cart.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 @Builder
 public class CartRequestDTO {
+
+    @NotBlank(message = "공백은 입력받을 수 없습니다.")
+    @Pattern(regexp = "^[0-9]*$", message = "상품 ID는 숫자만 입력받을 수 있습니다.")
     private Long productId;
 }
